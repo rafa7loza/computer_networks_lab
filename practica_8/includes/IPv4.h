5ff090e3a986317c20a8696c4aa98973f9b2047e
Rafael Loza <top-coder@Andres-MacBook-Air.local>
2020-05-07 19:55:19 -0500

    First commit practica8 (UDP).

---
+++ practica_8/includes/IPv4.h
@@ -0,0 +1,67 @@
+#ifndef IPV4_H
+#define IPV4_H
+
+#include <iostream>
+#include <string>
+
+#include "ICMPv4.h"
+#include "TCP.h"
+#include "helpers.h"
+
+// IPv4 Protocol
+#define VERSION "version"
+#define HEADER_LENGTH "length_header"
+#define SERVICE_TYPE "service"
+#define TOTAL_LENGTH "length_total"
+#define IDENTIFIER "id"
+#define FLAGS "flags"
+#define FRAGMENT_POSITION "fr_position"
+#define TTL "ttl"
+#define PROTOCOL "prot"
+#define CHECKSUM_HEADER "chksm_header"
+#define ADDRESS_ORIGIN "addr_o"
+#define ADDRESS_DEST "addr_d"
+#define PAYLOAD "payload"
+
+// Segment types
+#define ICMP_TYPE "ICMPv4"
+#define TCP_TYPE "TCP"
+
+using namespace std;
+
+struct service{
+  string priority;
+  string retard;
+  string performance;
+  string reliavility;
+};
+
+class IPv4{
+private:
+  // Attributes
+  string version;
+  int headerLength;
+  service serviceType;
+  int totalLength;
+  int id;
+  string flags;
+  int position;
+  int ttl;
+  string protocol;
+  string checksumHeader;
+  string originAddress;
+  string objectiveAddress;
+  string payloadType;
+  void * ptr = nullptr;
+
+  // Methods
+  void setField(string &data, string field, int * fpos);
+
+public:
+  IPv4();
+  IPv4(string &data, int *fposition);
+  void showData();
+  string getProtocol();
+};
+
+#endif
---
+++ practica_8/src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+#include "helpers.h"
+#include "Header.h"
+#include "IPv4.h"
+
+using namespace std;
+
+int main(){
+
+  int position = 0;
+  char fileName[] = "ethernet_ipv4_tcp_irc.bin";
+
+  cout << "\n\tReading datagram from: " << fileName << endl << endl;
+  string datagram = readPackage(fileName);
+
+  Header header = Header(datagram, &position);
+  header.showData();
+
+  if(header.getType() == IPV4_CODE){
+    IPv4 ipv4 = IPv4(datagram, &position);
+    ipv4.showData();
+  }
+
+  cout<<endl;
+
+  return 0;
+
+}
